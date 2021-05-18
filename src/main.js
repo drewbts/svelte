54fb4c799b554df85a78b5b343172a2575194dc4
drewbts <83302478+drewbts@users.noreply.github.com>
2021-05-18 22:44:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import App from './App.svelte';
+import {
+  Camera,
+  Group,
+  Scene,
+} from 'https://unpkg.com/three@0.117.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.117.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.117.0/examples/jsm/loaders/GLTFLoader.js';
 
 const app = new App({
 	target: document.body,
