97896206941a3d4b51787043936d6a02eb917a31
drewbts <83302478+drewbts@users.noreply.github.com>
2021-05-18 23:14:56 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,12 @@ import {
   Group,
   Scene,
 } from 'https://unpkg.com/three@0.117.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.117.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.117.0/examples/jsm/loaders/GLTFLoader.js';
+
 import {
-  BoxBufferGeometry,
-  Color,
-  Mesh,
-  MeshBasicMaterial,
   PerspectiveCamera,
-  Scene,
+  MeshStandardMaterial,
   WebGLRenderer,
-} from 'three';
+} from './vendor/three/build/three.module.js';
 
 const app = new App({
 	target: document.body,
